da3e3ab4506943647cff97368430fcf13af85366
Tharun G V <tharungv4@gmail.com>
2024-07-12 02:43:56 +0530

    sidebar-updated

--- app/(admin)/layout.js
+++ app/(admin)/layout.js
@@ -1,22 +1,30 @@
 'use client'
 import Navbar from "@/components/adminpages/Navbar";
-import Slidebar from "@/components/adminpages/Slidebar";
+import Slidebar, { SlidebarItem } from "@/components/adminpages/Slidebar";
 import { useState } from "react";
 import "../globals.css";
+import { BaggageClaim, Box, ClipboardList, LayoutDashboard, Settings } from "lucide-react";
 export default function RootLayout({ children }) {
   const [show,isshow]=useState(true)
   console.log(show);
     return (
       <>
       <div className=" flex  font-Text">
-        <Slidebar show={show} isshow={isshow}/>
+      
+        <Slidebar show={show} isshow={isshow}>
+        <SlidebarItem icon={<LayoutDashboard size={30}/>} text="Dashboard" active link="/dashboard"/>
+        <SlidebarItem icon={<BaggageClaim  size={30}/>} text="Products"  link="/dashboard/products"/>
+        <SlidebarItem icon={<ClipboardList size={30}/>} text="Orders"  link="/dashboard/orders"/>
+        <SlidebarItem icon={<Box size={30}/>} text="Categories"  link="/dashboard/categories"/>
+        <SlidebarItem icon={<Settings size={30}/>} text="Account"  link="/dashboard/products"/>
+        </Slidebar>
         
          <div className={
-          show?"w-full ":"w-full ml-64"
+          show?"w-full ":"w-full  ml-56"
          }>
         <Navbar show={show} isshow={isshow}/>
        
-        <main className=" mt-14   flex  px-3 py-4 mobile:ml-0  min-h-screen  bg-green-200 " >{children }</main>
+        <main className=" mt-14   ml-56 flex  px-3 py-4 mobile:ml-0  min-h-screen  bg-green-200 " >{children }</main>
         </div>
         </div>
 
--- components/adminpages/Slidebar.jsx
+++ components/adminpages/Slidebar.jsx
@@ -1,7 +1,7 @@
 "use client"
-import { ArrowLeft, LayoutDashboard, X, icons,ClipboardCheck, ArrowBigDown } from 'lucide-react'
+import { ArrowLeft, LayoutDashboard, X, icons,ClipboardCheck, ArrowBigDown, ChevronFirst, ChevronLast } from 'lucide-react'
 import Link from 'next/link'
-import React from 'react'
+import React, { createContext, useContext, useState } from 'react'
 import {
   Collapsible,
   CollapsibleContent,
@@ -14,79 +14,124 @@ import Image from 'next/image'
 import Analysischarts from './Analysischarts'
 
 
-export default function Slidebar({show,isshow}) {
-  const pathname = usePathname()
-  
-  const ProductLink = [
-    {
-      title:"Product List",
-      Link:"/dashboard/products"
-    },
-    {
-      title:"Create Product",
-      Link:"/dashboard/products"
-    },
-  ]
-  const CategoryLinks = [
-    {
-      title:"Product List",
-      Link:"/dashboard/products"
-    },
-    {
-      title:"Create Product",
-      Link:"/dashboard/products"
-    },
-  ]
-  const OrdersLinks = [
-    {
-      title:"Product List",
-      Link:"/dashboard/products"
-    },
-    {
-      title:"Create Product",
-      Link:"/dashboard/products"
-    },
-  ]
-console.log(ProductLink);
-  return (
-    
-    <div className={
-      show?` bg-[#2b3445] w-64  text-white  hidden z-50  h-dvh fixed     mobile:hidden  ${" mobile:block"}   overflow-y-auto `:" bg-[#2b3445] w-64   mobile:block     text-white   mobile:z-0  h-dvh fixed  overflow-y-auto "
-    }>
-      <div className=" ">
-          <div className="first-logo  flex  justify-between     p-10 text-white">
-<h1 className=' font-medium text-xl'><span className='text-green-500 text-3xl'>L</span>andson</h1>
-<div className="open-not-btn hover:rounded-full flex justify-center items-center w-10 h-10  hover:bg-slate-400  ">
- <button onClick={()=>isshow(true)}> <ArrowLeft /></button>
-</div>
-          </div>
-          <div className="dashboard-links flex text-xl  items-center flex-col gap-3">
-               <div className="dashboard ">
-               <Link href="/dashboard" className={pathname == "/dashboard"?" bg-[#373f50] p-2 rounded-lg text-blue-600    inline-flex gap-3":"inline-flex gap-3"} > <LayoutDashboard/> Dashboard</Link>
-               </div>
-               <div className="dashboard ">
-               <Link href="/dashboard/products" className={pathname == "/dashboard/products"?" bg-[#373f50] p-2 rounded-lg text-blue-600    inline-flex gap-3":"inline-flex gap-3"} > <LayoutDashboard/> Products</Link>
-               </div>
-               <Collapsible>
-  <CollapsibleTrigger>Products</CollapsibleTrigger>
-  <CollapsibleContent>
-  {
-    ProductLink.map((items,i)=>{
-      <Link href={items.Link} key={i} className={pathname == items.Link?" bg-[#373f50] p-2 rounded-lg text-blue-600   inline-flex gap-3":"inline-flex gap-3"} > <LayoutDashboard/> {items.title}</Link>
 
-    })
-  }
- 
-  </CollapsibleContent>
-</Collapsible>
+const SlidebarContext = createContext()
+export  function SlidebarItem({icon,text,active,alert, link}){
+ const{expand} = useContext(SlidebarContext)
+return(
+  <Link href={link} className={
+    `relative flex items-center py-2 px-3 my-1 rounded-md cursor-pointer  transition-colors${active?" bg-gradient-to-tr  from-indigo-100  to-indigo-200 text-indigo-800":" hover:bg-indigo-50 text-gray-600"}`
+  }>
+    {icon}
+    <span className={` overflow-hidden transition-all ${expand?"w-52 ml-3":"w-0"}`}>{text}</span>
+    {active&&<div className={` absolute right-2 w-2 h-2  rounded  bg-indigo-600 ${expand?"":" top-2"}` }></div>}
+  </Link>
+)
+}
+
+
+export default function Slidebar({children}) {
+  const [expand,setexpand]= useState(true)
+  // const pathname = usePathname()
+  
+  // const ProductLink = [
+  //   {
+  //     title:"Product List",
+  //     Link:"/dashboard/products"
+  //   },
+  //   {
+  //     title:"Create Product",
+  //     Link:"/dashboard/products"
+  //   },
+  // ]
+  // const CategoryLinks = [
+  //   {
+  //     title:"Product List",
+  //     Link:"/dashboard/products"
+  //   },
+  //   {
+  //     title:"Create Product",
+  //     Link:"/dashboard/products"
+  //   },
+  // ]
+  // const OrdersLinks = [
+  //   {
+  //     title:"Product List",
+  //     Link:"/dashboard/products"
+  //   },
+  //   {
+  //     title:"Create Product",
+  //     Link:"/dashboard/products"
+  //   },
+  // ]
+
+  return (
+    <aside className='h-screen  mobile:hidden   '>
+      <nav className=' h-full  flex-col border-r   fixed  bg-white z-50 w-56 shadow-sm'>
+        <div className="p-6 pb-2 flex justify-between items-center ">
+          <p className={` text-2xl ${expand?"":"hidden"}` } ><span className=' text-3xl text-green-400'>L</span>andson</p>
+          <button  onClick={()=>setexpand(curr=>!curr)}  className=' p-1.5   mx-16    rounded-lg bg-gray-50 hover:bg-gray-100'>
+            { expand?<ChevronFirst/>:<ChevronLast/>}
+          </button>
+        </div>
+        <SlidebarContext.Provider value={{expand}}>
+
+      
+        <ul className=' flex-1 px-3'>{children}</ul>
+        </SlidebarContext.Provider>
+
+      </nav>
+    </aside>
+//     <div className={
+//       show?` bg-[#2b3445] w-64  text-white  hidden z-50  h-dvh fixed     mobile:hidden  ${" mobile:block"}   overflow-y-auto `:" bg-[#2b3445] w-64   mobile:block     text-white   mobile:z-0  h-dvh fixed  overflow-y-auto "
+//     }>
+//       {/* <div className=" ">
+//           <div className="first-logo  flex  justify-between     p-10 text-white">
+// <h1 className=' font-medium text-xl'><span className='text-green-500 text-3xl'>L</span>andson</h1>
+// <div className="open-not-btn hover:rounded-full flex justify-center items-center w-10 h-10  hover:bg-slate-400  ">
+//  <button onClick={()=>isshow(true)}> <ArrowLeft /></button>
+// </div>
+//           </div>
+//           <div className="dashboard-links flex text-xl  w-full items-center flex-col gap-3">
+//                <div className="dashboard text-[15px]  ">
+//                <Link href="/dashboard" className={pathname == "/dashboard"?" bg-[#373f50]  p-2 rounded-lg text-blue-600    inline-flex gap-3":"inline-flex gap-3"} > <LayoutDashboard/> Dashboard</Link>
+//                </div>
+              
+//                <Collapsible>
+//   <CollapsibleTrigger>Products</CollapsibleTrigger>
+//   <CollapsibleContent>
+//   <div className="dashboard  px-3">
+//                <Link href="/dashboard/products/new" className={pathname == "/dashboard/products/new"?" bg-[#373f50] p-2 rounded-lg text-blue-600    inline-flex gap-3":"inline-flex gap-3"} > <LayoutDashboard/>Add Products</Link>
+//                <Link href="/dashboard/products" className={pathname == "/dashboard/products"?" bg-[#373f50] p-2 rounded-lg text-blue-600    inline-flex gap-3":"inline-flex gap-3"} > <LayoutDashboard/> Products List</Link>
+//                </div>
+//   </CollapsibleContent>
+// </Collapsible>
+// <Collapsible>
+//   <CollapsibleTrigger>Products</CollapsibleTrigger>
+//   <CollapsibleContent>
+//   <div className="dashboard  px-3">
+//                <Link href="/dashboard/products/new" className={pathname == "/dashboard/products/new"?" bg-[#373f50] p-2 rounded-lg text-blue-600    inline-flex gap-3":"inline-flex gap-3"} > <LayoutDashboard/>Add Products</Link>
+//                <Link href="/dashboard/products" className={pathname == "/dashboard/products"?" bg-[#373f50] p-2 rounded-lg text-blue-600    inline-flex gap-3":"inline-flex gap-3"} > <LayoutDashboard/> Products List</Link>
+//                </div>
+//   </CollapsibleContent>
+// </Collapsible>
+// <Collapsible>
+//   <CollapsibleTrigger>Products</CollapsibleTrigger>
+//   <CollapsibleContent>
+//   <div className="dashboard  px-3">
+//                <Link href="/dashboard/products/new" className={pathname == "/dashboard/products/new"?" bg-[#373f50] p-2 rounded-lg text-blue-600    inline-flex gap-3":"inline-flex gap-3"} > <LayoutDashboard/>Add Products</Link>
+//                <Link href="/dashboard/products" className={pathname == "/dashboard/products"?" bg-[#373f50] p-2 rounded-lg text-blue-600    inline-flex gap-3":"inline-flex gap-3"} > <LayoutDashboard/> Products List</Link>
+//                </div>
+//   </CollapsibleContent>
+// </Collapsible>
                
 
               
               
-</div>          
+// </div>          
 
-    </div>
-    </div>
+//     </div> */}
+//     </div>
     
   )
 }
