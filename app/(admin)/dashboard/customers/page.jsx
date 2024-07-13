4794e8887b6ccebb82718f630b75629bb3280133
Tharun G V <tharungv4@gmail.com>
2024-07-14 01:36:07 +0530

    prisma updated

--- app/(admin)/dashboard/customers/page.jsx
+++ app/(admin)/dashboard/customers/page.jsx
@@ -1,24 +1,28 @@
-import CustumData from '@/components/adminpages/CustunData'
-import Header from '@/components/adminpages/Header'
-import React from 'react'
-import  db from '@/lib/db'
-
-
+import CustumData from '@/components/adminpages/CustunData';
+import Header from '@/components/adminpages/Header';
+import React from 'react';
+import db from '@/lib/db';
 
 export default async function Dealer() {
-const users = await db.Users.findMany();
+  let users = [];
+
+  try {
+    users = await db.Users.findMany();
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    // Optionally, you can log this error to an external logging service
+  }
+
   return (
     <>
-   <div className=" flex flex-col">
-    <div className="">
-    <Header title="Customers" linkpage="/dashboard/customers/new" headtitle="Add Customer" />
-    </div>
-    <div className="table">
-    <CustumData data={users} title="Users List"/>
-    </div>
-    </div>
-
+      <div className="flex flex-col">
+        <div className="">
+          <Header title="Customers" linkpage="/dashboard/customers/new" headtitle="Add Customer" />
+        </div>
+        <div className="table">
+          <CustumData data={users} title="Users List"/>
+        </div>
+      </div>
     </>
-  )
+  );
 }
-
